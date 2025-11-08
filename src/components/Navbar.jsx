08874b1de3591507d9df48570dea4cf99e12a1c3
Rohith S <rohith-1205@gmail.com>
2025-11-08 18:38:37 +0530

    updated nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import logo from "../assets/logo.png";
+import logo from "FDtechlogo.png";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
